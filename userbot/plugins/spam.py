db0d481ef0bd6e9f6c95e5ed6bde977422bcaf2e
unknownforall1 <83235714+unknownforall1@users.noreply.github.com>
2021-06-26 10:34:24 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -16,13 +16,13 @@ async def spammer(event):
         return
     sandy = await event.get_reply_message()
     cat = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
-    counter = int(cat[10])
-    if counter > 5:
-        sleeptimet = 0.01
-        sleeptimem = 0.1
+    counter = int(cat[0])
+    if counter > 20:
+        sleeptimet = 0.04
+        sleeptimem = 0.8
     else:
         sleeptimet = 0.01
-        sleeptimem = 0.01
+        sleeptimem = 0.02
     await spam_function(event, sandy, cat, sleeptimem, sleeptimet)
 
 
